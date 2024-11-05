bb9b3f876da329e6f1a0c731a1677d1d70a5c8d3
Max Alberti <maxalberti@gmx.de>
2024-11-05 15:48:39 +0100

    Changed to gradle

---
+++ src/test/java/de/dhbw_ravensburg/webeng2/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw_ravensburg.webeng2.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
