8e91f25df4c5ebb5820b1fbc5165f73de8dbb98c
Max Alberti <maxalberti@gmx.de>
2024-11-26 11:02:00 +0100

    Added Books

---
+++ src/main/java/de/dhbw_ravensburg/webeng2/backend/controller/BookRepository.java
@@ -0,0 +1,14 @@
+package de.dhbw_ravensburg.webeng2.backend.controller;
+
+import java.util.List;
+import de.dhbw_ravensburg.webeng2.backend.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "books", path = "books")
+public interface BookRepository extends MongoRepository<Book, String> {
+
+  List<Book> findByName(@Param("name") String name);
+
+}
--- src/main/java/de/dhbw_ravensburg/webeng2/backend/model/Book.java
+++ src/main/java/de/dhbw_ravensburg/webeng2/backend/model/Book.java
@@ -1,15 +1,41 @@
 package de.dhbw_ravensburg.webeng2.backend.model;
 
-import org.springframework.context.annotation.Document;
-import org.springframework.context.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 
-@Document("Book")
+@Document("books")
 public class Book {
-    @Id
-    private int id;
+    @Id private String id;
+    private String name;
+    @Indexed(unique = true) private String isbn;
 
-    public Book(int id) {
+    public Book(String isbn, String name) {
         super();
-        this.id = id;
+        this.isbn = isbn;
+        this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Book[isbn=%s, name='%s']",
+            isbn, name);
+  }
 }
