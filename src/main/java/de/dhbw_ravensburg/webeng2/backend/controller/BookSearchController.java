26031d95bccbe3ac1e2e0a3dd66a3335f860e5b3
Max Alberti <maxalberti@gmx.de>
2024-11-28 12:34:02 +0100

    Changed Controller names

--- src/main/java/de/dhbw_ravensburg/webeng2/backend/controller/BookSearchController.java
+++ src/main/java/de/dhbw_ravensburg/webeng2/backend/controller/BookSearchController.java
@@ -1,5 +1,7 @@
 package de.dhbw_ravensburg.webeng2.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,27 +19,15 @@ import de.dhbw_ravensburg.webeng2.backend.repos.BookRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/api/books")
-public class BookController {
+@RequestMapping("/api/books/search")
+public class BookSearchController {
 
     @Autowired
     private BookRepository repository;
 
-    @GetMapping("/")
-    public Page<Book> findBooks() {
-        return repository.findAll(Pageable.ofSize(20));
-    }
-
-    @GetMapping("/{id}")
-    public Book findById(@PathVariable String id) {
-        return repository.findById(id).orElseThrow(() -> new BookNotFoundException());
-    }
-
-    @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Book updateBook(
-            @PathVariable("id") final String id, @RequestBody final Book book) {
-        return book;
+    @GetMapping("findByName/{name}")
+    public List<Book> findByName(@PathVariable String name) {
+        return repository.findByName(name);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
--- src/main/java/de/dhbw_ravensburg/webeng2/backend/repos/BookRepository.java
+++ src/main/java/de/dhbw_ravensburg/webeng2/backend/repos/BookRepository.java
@@ -5,6 +5,7 @@ import de.dhbw_ravensburg.webeng2.backend.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends MongoRepository<Book, String> {
