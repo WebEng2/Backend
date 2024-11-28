bac5bb1af92380d04208d7fa4667b85230cbb085
Max Alberti <maxalberti@gmx.de>
2024-11-28 13:19:43 +0100

    Found how to get rid of default API routes

--- src/main/java/de/dhbw_ravensburg/webeng2/backend/controller/BookController.java
+++ src/main/java/de/dhbw_ravensburg/webeng2/backend/controller/BookController.java
@@ -1,7 +1,5 @@
 package de.dhbw_ravensburg.webeng2.backend.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,14 +17,31 @@ import de.dhbw_ravensburg.webeng2.backend.repos.BookRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/api/books/search")
-public class BookSearchController {
+@RequestMapping("/api/books")
+public class BookController {
 
     @Autowired
     private BookRepository repository;
 
+    @GetMapping("/")
+    public Page<Book> findBooks() {
+        return repository.findAll(Pageable.ofSize(20));
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable String id) {
+        return repository.findById(id).orElseThrow(() -> new BookNotFoundException());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Book updateBook(
+            @PathVariable("id") final String id, @RequestBody final Book book) {
+        return book;
+    }
+
     @GetMapping("findByName/{name}")
-    public List<Book> findByName(@PathVariable String name) {
+    public Page<Book> findByName(@PathVariable String name) {
         return repository.findByName(name);
     }
 
